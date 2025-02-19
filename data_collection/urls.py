3f21b64b20f469a5c83198108a2c631cd6ebfa4b
Rounak2001 <rounakpatel2211@gmail.com>
2025-02-19 16:43:33 +0530

    minor change

--- data_collection/urls.py
+++ data_collection/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('create_farmer/', create_farmer, name='create_farmer'),
+    path('', create_farmer, name='create_farmer'),
     path('add_farm/<int:farmer_id>/', add_farm, name='add_farm'),
     path('add-plantation/<int:farm_id>/', add_plantation, name='add_plantation'),
     path('add-species/<int:plantation_id>/', add_specie, name='add_specie'),
