53a043bc4b8e83dd231ac40a99d0ea7c718f0ca3
Rounak2001 <rounakpatel2211@gmail.com>
2025-02-22 15:39:35 +0530

    admin panel added

--- data_collection/admin.py
+++ data_collection/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Farmer
 
-# Register your models here.
+class FarmerAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "aadhar", "consent_form")  # ✅ Show file field
+
+admin.site.register(Farmer, FarmerAdmin)
